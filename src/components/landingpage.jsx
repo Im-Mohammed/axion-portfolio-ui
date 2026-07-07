9f87e80cfb33adb2a1af411561e79413cb408085
Im-Mohammed <mohammedkaarab.777@gmail.com>
2026-07-07 20:26:05 +0530

    Fix : LandingPage spline component

--- src/components/landingpage.jsx
+++ src/components/landingpage.jsx
@@ -1,43 +1,43 @@
-import { memo } from 'react';
-import Spline from '@splinetool/react-spline';
-import TextPressure from '../reactbits/TextPressure/TextPressure.jsx';
-import TrueFocus from '../reactbits/TrueFocus/TrueFocus.jsx';
 import './landingpage.css';
+import Spline from '@splinetool/react-spline';
+import TextPressure from '../reactbits/TextPressure/TextPressure.jsx';    
+import TrueFocus from '../reactbits/TrueFocus/TrueFocus.jsx';    
 
-export const LandingPage = memo(() => {
-
+export const LandingPage = () => {
   return (
-    <section id="home" className="landing-section">
-      <div className="landing-container">
-        <div className="spline-scene">
-          {/* Only render Spline when landing section is visible */}
-          <Spline scene="/spline/scene.splinecode" />
-        </div>
-        <div className="variable-text">
-          <div style={{ position: 'relative', height: '200px' }}>
-            <TextPressure
-              text="Mohammed  Karab  Ehtesham"
-              flex={true}
-              alpha={false}
-              stroke={false}
-              width={true}
-              weight={false}
-              italic={false}
-              textColor="#ffffff"
-              strokeColor="#ff0000"
-              minFontSize={25}
-            />
-          </div>
-          <TrueFocus
-            sentence="Welcome to My Portfolio"
-            manualMode={false}
-            blurAmount={10}
-            borderColor="dark violet"
-            animationDuration={2}
-            pauseBetweenAnimations={1}
+    <section id = "home" className="landing-section">
+    <div className="landing-container">
+      <div className="spline-scene">
+        <Spline scene="/spline/scene.splinecode" />
+      </div>
+
+      {/* Overlay text */}
+      <div className="variable-text">
+        <div style={{position: 'relative', height: '200px'}}>
+          <TextPressure
+           text="Mohammed  Karab  Ehtesham"
+           flex={true}
+           alpha={false}
+           stroke={false}
+           width={true}
+           weight={false}
+           italic={false}
+           textColor="#ffffff"
+            strokeColor="#ff0000"
+            minFontSize={25}
           />
         </div>
+        <TrueFocus 
+          sentence="Welcome to My Portfolio"
+          manualMode={false}
+          blurAmount={10}
+          borderColor="dark violet"
+          animationDuration={2}
+          pauseBetweenAnimations={1}
+        />
       </div>
-    </section>
+    </div>
+   </section>
   );
-});
+};
+
